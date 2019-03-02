2f34426e51ab3a2dc48f6dddc1458508d4e0af37
Atif Aslam <aslam.renu.soft43@gmail.com>
2019-03-01 19:56:49 -0800

    update completed

--- Database-JPA/src/main/java/com/renu/jpa/many_to_many/controllers/ManyToMany.java
+++ Database-JPA/src/main/java/com/renu/jpa/many_to_many/controllers/ManyToMany.java
@@ -25,42 +25,43 @@ public class ManyToMany {
 
 	@Autowired
 	TeacherRepository teacherRepository;
-@Autowired
-StudentRepository studentRepository;
-//-----------------------------------------------------------------------------------------------------------------
-	
-	//----------------------------------------------------------------------------------------------------------------
-	@RequestMapping(value="/add")
-	public ResponseEntity<?>addManyToMany(@RequestBody TeacherStudentCombined teacherStudentCombined){
+	@Autowired
+	StudentRepository studentRepository;
+	// -----------------------------------------------------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------------------------------------------------
+	@RequestMapping(value = "/add")
+	public ResponseEntity<?> addManyToMany(@RequestBody TeacherStudentCombined teacherStudentCombined) {
 		LOGGER.info("From class ManyToMany,method : addManyToMany()--ENTER----");
-		teacherStudentCombined.getTeachers().forEach(x->x.setStudents(teacherStudentCombined.getStudents()));
+		teacherStudentCombined.getTeachers().forEach(x -> x.setStudents(teacherStudentCombined.getStudents()));
 		teacherRepository.saveAll(teacherStudentCombined.getTeachers());
-		
+
 		return ResponseEntity.ok().body("----many-to-many-added----");
 	}
-	//----------------------------------------------------------------------------------------------------------------
-		@RequestMapping(value="/getAll")
-		public ResponseEntity<?>getAll(){
-			LOGGER.info("From class ManyToMany,method : getAll()--ENTER----");
-			List<Teacher>teachers=teacherRepository.findAll();
-			return ResponseEntity.ok().body(teachers);
-		}
-		//----------------------------------------------------------------------------------------------------------------
-				@RequestMapping(value="/get/{id}")
-				public ResponseEntity<?>getById(@PathVariable("id") Long id){
-					LOGGER.info("From class ManyToMany,method : getById()--ENTER----");
-					Teacher teachers=teacherRepository.getById(id);
-					return ResponseEntity.ok().body(teachers);
-				}
-				//----------------------------------------------------------------------------------------------------------------
-				@RequestMapping(value="/delete/{id}")
-				public ResponseEntity<?>deleteById(@PathVariable("id") Long id){
-					LOGGER.info("From class ManyToMany,method : deleteById()--ENTER----");
-					Teacher teacher=teacherRepository.getById(id);
-					teacherRepository.delete(teacher);
-					return ResponseEntity.ok().body("---DELETED ID : "+id);
-				}
-				
-	
+
+	// ----------------------------------------------------------------------------------------------------------------
+	@RequestMapping(value = "/getAll")
+	public ResponseEntity<?> getAll() {
+		LOGGER.info("From class ManyToMany,method : getAll()--ENTER----");
+		List<Teacher> teachers = teacherRepository.findAll();
+		return ResponseEntity.ok().body(teachers);
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	@RequestMapping(value = "/get/{id}")
+	public ResponseEntity<?> getById(@PathVariable("id") Long id) {
+		LOGGER.info("From class ManyToMany,method : getById()--ENTER----");
+		Teacher teachers = teacherRepository.getById(id);
+		return ResponseEntity.ok().body(teachers);
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	@RequestMapping(value = "/delete/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
+		LOGGER.info("From class ManyToMany,method : deleteById()--ENTER----");
+		Teacher teacher = teacherRepository.getById(id);
+		teacherRepository.delete(teacher);
+		return ResponseEntity.ok().body("---DELETED ID : " + id);
+	}
 
 }
